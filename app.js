d0a0d6d52edbcef52fc4a55b8738aaf92c5e380c
Ajay Spak <ajayspakcommcom>
2025-12-24 18:02:34 +0530

    updated

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 // app.js
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello from Express!!!!');
 });
 
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
 app.get('/about', (req, res) => {
-    res.send('Hello from About!');
-  });
+  res.send('Hello from About!');
+});
 
-  app.get('/contact', (req, res) => {
-    res.send('Hello from About!');
-  });
+app.get('/contact', (req, res) => {
+  res.send('Hello from About!');
+});
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on port ${port}`);
 });
